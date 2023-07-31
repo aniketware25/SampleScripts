d4766e3f56e72f9995dea158ee482cbdaa458b5e
Aniket Ware <aware@mediaocean.com>
2023-07-31 16:38:30 +0530

    adding cooments to scripts

--- addition.sh
+++ addition.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#this is addition of two numbers 
-
+#this is addition of two numbers by entering on keypad
 echo "enter first number : " 
 read num1
 echo "enter second number : "
 read num2 
 sum=$(($num1+$num2))
 echo "sum is : $sum"
+
+
+

033746f39977a34750e4ee3f9679a3cc950e8bb0
Aniket Ware <aware@mediaocean.com>
2023-08-02 21:07:07 +0530

    added comments in script

--- day2_bashScript_backupfile.sh
+++ day2_bashScript_backupfile.sh
@@ -3,16 +3,17 @@
 
  tar -cvf $1/backup_"$(date +%Y-%m-%d_%H-%M-%S)".gz $1/*
 
-# count the number of backup files in folder
+# Initial count the number of backup files in folder
 count=$( ls -lt backup* | wc -l )
 if [ $count -gt 3 ]
 then echo "More than 3 files located in folder, Actual count= $count "
-echo deleting old files 
+# deleting extra oldest files 
 while [ $count -gt 3 ]
 do 
 fileDeleted=$( ls -t backup* | tail -1 )
 echo "Deleting file $fileDeleted"
 rm $fileDeleted
+#reduce count for checking next oldest file
 count=$( ls -lt backup* | wc -l )
 done
 fi
